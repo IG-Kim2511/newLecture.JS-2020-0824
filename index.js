6623e3052c2c35d718dbb442e95ffd36c418a1ab
IG Kim <59094101+IanKim2511@users.noreply.github.com>
2020-08-25 14:24:52 -0400

    index arrange

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-//window.onload = function(){
-window.addEventListener("load", function(){
-    var btnPrint = document.getElementById("btn-print");
+//c19. window.onload = function(){
+// window.addEventListener("load", function(){
+//     var btnPrint = document.getElementById("btn-print");
 
-    var add = function(x, y){
-        return x + y;
-    };
+//     var add = function(x, y){
+//         return x + y;
+//     };
 
-    btnPrint.onclick = function () {               
+//     btnPrint.onclick = function () {               
 
-        var x = prompt("x 값을 입력하세요", 0);
-        var y = prompt("y 값을 입력하세요", 0);
+//         var x = prompt("x 값을 입력하세요", 0);
+//         var y = prompt("y 값을 입력하세요", 0);
         
-        x = parseInt(x);
-        y = parseInt(y);            
-        btnPrint.value = x+y;            
-    };
-});
+//         x = parseInt(x);
+//         y = parseInt(y);            
+//         btnPrint.value = x+y;            
+//     };
+// });
