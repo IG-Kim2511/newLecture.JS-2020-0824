401c4da7799b86336ff47547628636d1144b83af
IG Kim <59094101+IanKim2511@users.noreply.github.com>
2020-08-25 12:26:03 -0400

    c41.Ex2- event bubbling 을 이용한 사용자 이벤트 처리하기

--- javascript2-event.js
+++ javascript2-event.js
@@ -1,37 +1,6 @@
-// Ex3-이벤트 버블링 멈추기
+//Ex11-선택된 이미지 보여주기:event target
 window.addEventListener("load", function () {
-  var section = document.querySelector("#section3");
-
-  var imgList = section.querySelector(".img-list");
-  var addButton = section.querySelector(".add-button");
-  var currentImg = section.querySelector(".current-img");
-
-  imgList.onclick = function (e) {};
-
-  addButton.onclick = function (e) {};
-});
-
-//Ex2- event bubbling 을 이용해 사용자 이벤트 처리하기:event Bubbling
-window.addEventListener("load", function () {
-  var section = document.querySelector("#section2");
-  var imgList = section.querySelector(".img-list");
-  var currentImg = section.querySelector(".current-img");
-
-  imgList.onclick = function (e) {
-    //   console.log('test');
-    //   console.log(e.target.nodeName);
-
-    if (e.target.nodeName != "IMG") return;
-    currentImg.src = e.target.src;
-  };
-});
-
-//연습문제 1-선택된 레코드 삭제하기:event target
-window.addEventListener("load", function () {});
-
-//Ex1-선택된 이미지 보여주기:event target
-window.addEventListener("load", function () {
-  var section = document.querySelector("#section1");
+  var section = document.querySelector("#section11");
 
   var imgs = section.querySelectorAll(".img");
   var currentImg = section.querySelector(".current-img");
@@ -57,4 +26,46 @@ window.addEventListener("load", function () {
 
   //3w
   // c41강  event bubbling  ㄱㄱ
+});
+
+//연습문제 11-1-선택된 레코드 삭제하기:event target
+window.addEventListener("load", function () {});
+
+//Ex12- event bubbling 을 이용해 사용자 이벤트 처리하기:event Bubbling
+window.addEventListener("load", function () {
+  var section = document.querySelector("#section12");
+  var imgList = section.querySelector(".img-list");
+  var currentImg = section.querySelector(".current-img");
+
+  imgList.onclick = function (e) {
+    //   console.log('test');
+    //   console.log(e.target.nodeName);
+
+    if (e.target.nodeName != "IMG") return;
+    currentImg.src = e.target.src;
+  };
+});
+
+// Ex3-이벤트 버블링 멈추기
+window.addEventListener("load", function () {
+  var section = document.querySelector("#section13");
+
+  var imgList = section.querySelector(".img-list");
+  var addButton = section.querySelector(".add-button");
+  var currentImg = section.querySelector(".current-img");
+
+  imgList.onclick = function (e) {
+    console.log("imgList.onclick");
+    if (e.target.nodeName != "IMG") return;
+    currentImg.src = e.target.src;
+  };
+
+  addButton.onclick = function (e) {
+    e.stopPropagation();
+    console.log("addButton.onclick");
+
+    let img = document.createElement("img");
+    img.src = "images/img1.jpg";
+    currentImg.insertAdjacentElement("afterend", img);
+  };
 });
